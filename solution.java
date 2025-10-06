c24554ffce6c3e4515d77ed17877ba4d23a0e2ad
wonder420 <84724922+wonder-420@users.noreply.github.com>
2025-10-06 13:21:04 +0200

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
